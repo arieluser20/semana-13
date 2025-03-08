4b853be5d8b4afae76eb66cc3374ad7a2d955a7c
arieluser20 <ariel.ponton2005@gmail.com>
2025-03-07 19:22:10 -0500

    cambios

--- tarea semana 13.py
+++ tarea semana 13.py
@@ -1,2 +1,10 @@
 #elfe
 print("hol mundo")
+
+temperaturas =[["Ambato","Lunes",23],]
+
+for filas in temperaturas:
+    for temperatura in filas:
+        if temperaturas == "Ambato":
+            sumar
+            
